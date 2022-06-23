c4a72bfedd86b59362c400150fa7174ccbe38005
Wajih <hass_wajih@hotmail.com>
2022-06-23 17:58:50 -0400

    make it better

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './Home'
+import PlaceIndex from './places/PlaceIndex'
+import PlaceDetails from './places/PlaceDetails'
+import Navigation from './Navigation'
+import Error404 from './Error404'
+import NewPlaceForm from './places/NewPlaceForm'
+import EditPlaceForm from './places/EditPlaceForm'
+import SignUpForm from './users/SignUpForm'
+import LoginForm from './users/LoginForm'
+import CurrentUserProvider from './contexts/CurrentUser'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CurrentUserProvider>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route  path="/" component={Home} />
+          <Route  path="/sign-up" component={SignUpForm} />
+          <Route  path="/login" component={LoginForm} />
+          <Route  path="/places" component={PlaceIndex} />
+          <Route  path="/places/new" component={NewPlaceForm} />
+          <Route  path="/places/:placeId" component={PlaceDetails} />
+          <Route  path="/places/:placeId/edit" component={EditPlaceForm} />
+          <Route path="/" component={Error404} />
+        </Routes>
+      </BrowserRouter>
+    </CurrentUserProvider>
   );
 }
 

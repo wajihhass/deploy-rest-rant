afc83465832901a53c6eae87d55a4e867c87ee8b
Wajih <hass_wajih@hotmail.com>
2022-06-24 19:20:22 -0400

    make it better

--- src/contexts/CurrentUser.js
+++ src/contexts/CurrentUser.js
@@ -20,8 +20,6 @@ function CurrentUserProvider({ children }){
         getLoggedInUser()
     }, [])
 
-    //window.setCurrentUser=setCurrentUser
-
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}

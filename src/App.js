b2cf407438907c39539075d6e6387cfab6a8ca9e
Wajih <hass_wajih@hotmail.com>
2022-06-24 17:25:32 -0400

    make it better

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import React from "react"
+//import ReactDOM from "react-dom/client";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
-import PlaceIndex from './places/PlaceIndex'
-import PlaceDetails from './places/PlaceDetails'
-import Navigation from './Navigation'
-import Error404 from './Error404'
-import NewPlaceForm from './places/NewPlaceForm'
-import EditPlaceForm from './places/EditPlaceForm'
-import SignUpForm from './users/SignUpForm'
-import LoginForm from './users/LoginForm'
-import CurrentUserProvider from './contexts/CurrentUser'
+import PlaceIndex from "./places/PlaceIndex";
+import PlaceDetails from "./places/PlaceDetails";
+import Navigation from "./Navigation";
+import Error404 from "./Error404";
+import NewPlaceForm from "./places/NewPlaceForm";
+import EditPlaceForm from "./places/EditPlaceForm";
+import SignUpForm from "./users/SignUpForm";
+import LoginForm from "./users/LoginForm";
+import CurrentUserProvider from "./contexts/CurrentUser";
 
 
 function App() {

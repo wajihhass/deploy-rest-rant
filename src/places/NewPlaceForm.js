a20dce5c9944fcaa35dad0d04db79495c046f46b
Wajih <hass_wajih@hotmail.com>
2022-06-24 18:10:44 -0400

    make it better

--- src/places/NewPlaceForm.js
+++ src/places/NewPlaceForm.js
@@ -1,6 +1,6 @@
 import { useState } from "react"
 //import { useHistory } from "react-router-dom"
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from "react-router";
 
 /*
 import { useNavigate } from 'react-router-dom';

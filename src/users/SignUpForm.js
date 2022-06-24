637458fdae9903c9cd9cbfacf6b52a96fdfed8c4
Wajih <hass_wajih@hotmail.com>
2022-06-23 22:45:10 -0400

    make it better

--- src/users/SignUpForm.js
+++ src/users/SignUpForm.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 //import { useHistory, useParams } from "react-router"
 import { useNavigate } from 'react-router-dom'
 
